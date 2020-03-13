21afc36956d5138ec27cb1d761a9eddb18647f7f
Vikas Dodera  Thimmaiah <>
2020-03-13 17:08:10 +0100

    Final Commit Final

--- reiselust-app/src/App.js
+++ reiselust-app/src/App.js
@@ -1,49 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
-//import Travel from './NatureBackground.mp4';
-import Logo from './wonderlust.png';
+import { MDBBtn } from "mdbreact";
+import UserForm from "./components/UserForm";
+import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Footer from './components/Footer';
+import ParisTrip from './components/ParisTrip';
+import Cards from './components/card';
 
 function App() {
   return (
     <div className="App">
-      <div className="smooth">
-      <header className="App-header">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#home"><img id="logoImg" src={Logo}></img></a></li>
-          <li><a href="#price">Price</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </header>
-      
-      
-      <section id="home">
-        <header className="v-header container">
-        {/* <div className="fullscreen-video-wrap">
-          <video src={Travel} autoPlay={true} loop={true} muted={true}></video>
-        </div> */}
 
-        <div className="header-overlay"></div>
+        {/* <Router>
+          
+          <Switch>
+              <Route path="/" exact component={Cards}/> 
+              {/* <Route path="/home" exact component={HomeComponent}/>  */}
+               
+          
+        
 
-        <div className="header-content">
-          <a href="#" className="btn btn-white btn-animated">Book Now</a>
-          <a href="#" className="btn btn-white btn-animated">Create Your own Trip</a>
-          <h1>Hello to Wonderlust Tours and Travels</h1>
-        </div>
-        </header>
-      </section>
-      <section id="about">
-          <h2>About Page</h2>
-      </section>
-      <section id="price">
-          <h2>Price Page</h2>
-      </section>
-      <section id="contact">
-          <h2>Contact Page</h2>
-      </section>
-      </div>
+
+    {/* <Navbar></Navbar>
+    <Cards></Cards>
+    <ParisTrip></ParisTrip>
+    <Footer></Footer> */}
+    
+     <Router>
+   
+        <Navbar></Navbar>
+        <Switch>
+         <Route path="/" exact component={Home}></Route>
+         <Route path="/ownTrip" exact component={UserForm}></Route>
+         <Route path="/packages/" component={Cards}/>
+         <Route path="/trip/" component={ParisTrip}/>
+         </Switch>
+        <Footer></Footer>
+      
+     </Router>
 
     </div>
   );
